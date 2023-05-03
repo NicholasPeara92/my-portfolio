adae01a450d704b840b86e7b9f9bfb5b7f17e25c
NicholasPeara92 <nicholas.peara@gmail.com>
2023-05-03 18:02:00 +0200

    Add profile card and socials

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="card card1">
+    <div class="border">
+      <h2>I miei social</h2>
+      <div class="icons">
+        <a
+          href="https://www.instagram.com/nicholasp_92/"
+          class="icon icon--instagram"
+        >
+          <i class="fa fa-instagram icon" aria-hidden="true"></i>
+        </a>
+        <a href="https://github.com/NicholasPeara92" class="icon icon--github">
+          <i class="fa fa-github icon" aria-hidden="true"></i>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/nicholas-peara-451010210/"
+          class="icon icon--linkedin"
+        >
+          <i class="fa fa-linkedin icon" aria-hidden="true"> </i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.border {
+  height: 600px;
+  width: 480px;
+  background: transparent;
+  border-radius: 10px;
+  transition: border 1s;
+  position: relative;
+}
+
+.border:hover {
+  border: 1px solid #000000;
+}
+
+h2 {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  color: #737373;
+  margin: 25px;
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+h2:hover {
+  color: #8f6e5d;
+  cursor: pointer;
+}
+
+.fa {
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+.card {
+  height: 620px;
+  width: 500px;
+  background: #808080;
+  border-radius: 10px;
+  transition: background 0.8s;
+  overflow: hidden;
+  background: #000;
+  box-shadow: 0 70px 63px -60px #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.card1 {
+  background: url("../assets/img4.jpeg") center center no-repeat;
+  background-size: 620px;
+}
+
+.card1:hover {
+  background: url("../assets/img4.jpeg") left center no-repeat;
+  background-size: 900px;
+}
+
+.card1:hover h2 {
+  opacity: 1;
+}
+
+.card1:hover .fa {
+  opacity: 1;
+}
+.card1:hover a {
+  outline: 2px solid;
+  transition-property: outline-offset, outline-color, background-color;
+  transition-duration: 0.25s;
+}
+a {
+  text-align: center;
+  color: #737373;
+  text-decoration: none;
+  font-size: 26px;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 50%;
+  margin: 15px;
+}
+.icons {
+  font-size: 32px;
+  position: absolute;
+  fill: #737373;
+  color: #737373;
+  height: 150px;
+  top: 226px;
+  left: 20px;
+  width: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.icon:hover {
+  color: white;
+  cursor: pointer;
+}
+.icon:hover {
+  outline-offset: 4px;
+}
+.icon i {
+  margin: auto;
+}
+.icon:hover i {
+  animation: shake 0.25s;
+}
+.icon--instagram:hover {
+  background-image: radial-gradient(
+    circle at 30% 107%,
+    #fdf497 0%,
+    #fdf497 5%,
+    #fd5949 45%,
+    #d6249f 60%,
+    #285aeb 90%
+  );
+  outline-color: #a02d76;
+}
+.icon--twitter:hover {
+  background-color: #1da1f2;
+  outline-color: #1da1f2;
+}
+.icon--linkedin:hover {
+  background-color: #0077b5;
+  outline-color: #0077b5;
+}
+.icon--github:hover {
+  background-color: #2ea44f;
+  outline-color: #2ea44f;
+}
+@keyframes shake {
+  10% {
+    transform: rotate(15deg);
+  }
+  20% {
+    transform: rotate(-15deg);
+  }
+  30% {
+    transform: rotate(15deg);
+  }
+  40% {
+    transform: rotate(-15deg);
+  }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,18 +6,27 @@
           <img src="../assets/heroLogo.png" alt="" />
           <a href="../assets/CV.pdf" download>SCARICA CV</a>
         </div>
+        <div class="col d-flex justify-content-end align-items-center">
+          <ProfileCard />
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import ProfileCard from "../components/ProfileCard.vue";
 export default {
-  name: "HomePage",
+  components: {
+    ProfileCard,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.home {
+  margin-top: 80px;
+}
 img {
   max-width: 800px;
 }
